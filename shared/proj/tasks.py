5a9dab56bb21cdb6a5e12a38642ddd96cfc73848
Fabian Persson <fabianpersson@hotmail.se>
2018-10-12 14:27:46 +0200

    first commit

---
+++ shared/proj/tasks.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, unicode_literals
+from .celery import app
+from .wordcount import WordCount
+import json
+import os
+
+@app.task
+def wordcounter(search_words=False):
+    if not search_words:
+        search_words = ["han", "hon", "hen", "den", "denne", "denna"] 
+
+    vc = WordCount(search_words)
+    
+    path = '/home/ubuntu/data/'
+
+    for f in os.listdir(path):
+        filename = os.path.join(path, f)
+        with open(filename) as file:
+            for line in file:
+                if (line != '\n'):
+                    tweet = json.loads(line)
+                    vc.update_counter(tweet['text'])
+                    
+    return vc.tot_counter
